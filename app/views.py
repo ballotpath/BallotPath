442c819615745dc3e05673dd9bbf47305bd89d14
Andrew Hobbs <firedraco2@yahoo.com>
2014-06-22 00:33:15 +0000

    Added function to display a JSON file for all districts for testing. Updated function to display a JSON file for a particular office_id. Added function to display a JSON file for all offices; needs to work with latitude and longitude though which will require a different query.

--- app/views.py
+++ app/views.py
@@ -5,13 +5,39 @@ import json
 @app.route("/")
 @app.route("/index")
 def index():
-    return '<a href="'+url_for('get_office', office_id=1)+'">Try looking for Office with id 1</a>'
+    return '<a href="'+url_for('get_office', office_id=1)+'">Try looking for Office with id 1</a><br /><a href="'+url_for('get_districts')+'">Try looking for all Districts</a>'
+
+@app.route("/office/<lat>/<long>/")
+def get_offices(lat, long):
+    offices = models.Office.query.all()
+    office_dicts = []
+    for office in offices:
+        office_dicts.append(dict(office.__dict__))
+    for office_dict in office_dicts:
+        del office_dict['_sa_instance_state']
+    return json.dumps(office_dicts)
 
 @app.route("/office/<office_id>/")
 def get_office(office_id):
-    # Use our model to connect to the DB
-    offices = models.Office.query.get(office_id)
-    if offices == None:
-        return json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+    # Use our model to connect to the DB and get our particular office
+    office = models.Office.query.get(office_id)
+    if office == None:
+        return json.dumps([])
     else:
-        return offices
+        # Make a copy of the dictionary
+        office_dict = dict(office.__dict__)
+        # Remove the extra SQLAlchemy data
+        del office_dict['_sa_instance_state']
+        # Then we can just use the json.dumps method to dump the python
+        # dictionary directly to a string
+        return json.dumps(office_dict)
+
+@app.route("/district/")
+def get_districts():
+    districts = models.District.query.all()
+    district_dicts = []
+    for district in districts:
+        district_dicts.append(dict(district.__dict__))
+    for district_dict in district_dicts:
+        del district_dict['_sa_instance_state']
+    return json.dumps(district_dicts)
