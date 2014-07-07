cefcaade7f0253c46510a51da142234cee5c2bb3
Andrew Hobbs <firedraco2@yahoo.com>
2014-07-07 20:38:38 +0000

    Fixed some errors in parse_office_row that prevented it from returning a result due to the query result being different than expected.

--- app/views.py
+++ app/views.py
@@ -12,50 +12,50 @@ def index():
 def parse_office_row(row):
     # First, extract office position information from the query
     office_pos = {}
-    office_pos['id'] = row[models.office_position.id]
-    office_pos['district_id'] = row[models.office_position.district_id]
-    office_pos['office_id'] = row[models.office_position.office_id]
-    office_pos['office_holder_id'] = row[models.office_position.office_holder_id]
-    office_pos['position_name'] = row[models.office_position.position_name]
-    office_pos['term_start'] = row[models.office_position.term_start]
-    office_pos['term_end'] = row[models.office_position.term_end]
-    office_pos['filing_deadline'] = row[models.office_position.filing_deadline]
-    office_pos['next_election'] = row[models.office_position.next_election]
-    office_pos['notes'] = row[models.office_position.notes]
+    office_pos['id'] = row.office_position.id
+    office_pos['district_id'] = row.office_position.district_id
+    office_pos['office_id'] = row.office_position.office_id
+    office_pos['office_holder_id'] = row.office_position.office_holder_id
+    office_pos['position_name'] = row.office_position.position_name
+    office_pos['term_start'] = str(row.office_position.term_start)
+    office_pos['term_end'] = str(row.office_position.term_end)
+    office_pos['filing_deadline'] = str(row.office_position.filing_deadline)
+    office_pos['next_election'] = str(row.office_position.next_election)
+    office_pos['notes'] = row.office_position.notes
     # Next, extract office information
     office = {}
-    office['id'] = row[models.office.id]
-    office['title'] = row[models.office.title]
-    office['num_positions'] = row[models.office.num_positions]
-    office['responsibilities'] = row[models.office.responsibilities]
-    office['term_length_months'] = row[models.office.term_length_months]
-    office['filing_fee'] = row[models.office.filing_fee]
-    office['partisan'] = row[models.office.partisan]
-    office['age_requirements'] = row[models.office.age_requirements]
-    office['res_requirements'] = row[models.office.res_requirements]
-    office['prof_requirements'] = row[models.office.prof_requirements]
-    office['salary'] = row[models.office.salary]
-    office['notes'] = row[models.office.notes]
+    office['id'] = row.office.id
+    office['title'] = row.office.title
+    office['num_positions'] = row.office.num_positions
+    office['responsibilities'] = row.office.responsibilities
+    office['term_length_months'] = row.office.term_length_months
+    office['filing_fee'] = row.office.filing_fee
+    office['partisan'] = row.office.partisan
+    office['age_requirements'] = row.office.age_requirements
+    office['res_requirements'] = row.office.res_requirements
+    office['prof_requirements'] = row.office.prof_requirements
+    office['salary'] = row.office.salary
+    office['notes'] = row.office.notes
     # Put it in as a sub-object
     office_pos['office'] = office
     # Finally, extract office_holder information
     office_holder = {}
-    office_holder['id'] = row[models.office_holder.id]
-    office_holder['first_name'] = row[models.office_holder.first_name]
-    office_holder['middle_name'] = row[models.office_holder.middle_name]
-    office_holder['last_name'] = row[models.office_holder.last_name]
-    office_holder['party_affiliation'] = row[models.office_holder.party_affiliation]
-    office_holder['address1'] = row[models.office_holder.address1]
-    office_holder['address2'] = row[models.office_holder.address2]
-    office_holder['city'] = row[models.office_holder.city]
-    office_holder['state'] = row[models.office_holder.state]
-    office_holder['zip'] = row[models.office_holder.zip]
-    office_holder['phone'] = row[models.office_holder.phone]
-    office_holder['fax'] = row[models.office_holder.fax]
-    office_holder['email_address'] = row[models.office_holder.email_address]
-    office_holder['website'] = row[models.office_holder.website]
-    office_holder['photo_link'] = row[models.office_holder.photo_link]
-    office_holder['notes'] = row[models.office_holder.notes]
+    office_holder['id'] = row.office_holder.id
+    office_holder['first_name'] = row.office_holder.first_name
+    office_holder['middle_name'] = row.office_holder.middle_name
+    office_holder['last_name'] = row.office_holder.last_name
+    office_holder['party_affiliation'] = row.office_holder.party_affiliation
+    office_holder['address1'] = row.office_holder.address1
+    office_holder['address2'] = row.office_holder.address2
+    office_holder['city'] = row.office_holder.city
+    office_holder['state'] = row.office_holder.state
+    office_holder['zip'] = row.office_holder.zip
+    office_holder['phone'] = row.office_holder.phone
+    office_holder['fax'] = row.office_holder.fax
+    office_holder['email_address'] = row.office_holder.email_address
+    office_holder['website'] = row.office_holder.website
+    office_holder['photo_link'] = row.office_holder.photo_link
+    office_holder['notes'] = row.office_holder.notes
     # And also add it as a sub-object
     office_pos['office_holder'] = office_holder
     return office_pos
@@ -64,10 +64,8 @@ def parse_office_row(row):
 @app.route("/office/<float:latitude>/<float:longitude>/")
 def get_offices(latitude, longitude, methods = ['GET']):
     office_positions = []
-    # Using a stored procedure
-    # result = db.engine.execute("SELECT * FROM STORED_PROCEDURE() JOIN office_position op ON sp.district_id = op.district_id JOIN office o ON op.office_id = o.id JOIN office_holder oh ON op.office_holder_id = oh.id")
     # For now, just query all office_positions
-    result = db.engine.execute("SELECT * FROM office_position op JOIN office o ON op.office_id = o.id JOIN office_holder oh ON op.office_holder_id = oh.id")
+    result = db.session.query(models.office, models.office_holder, models.office_position).filter(models.office.id == models.office_position.office_id).filter(models.office_holder.id == models.office_position.id).all()
     for row in result:
         office_positions.append(parse_office_row(row))
     return Response(json.dumps({ "office_positions" : office_positions }), status=200, mimetype='application/json')
