175392b2743555fcba650aa0ecad8d93bbfb48d7
mclyde <mclyde122@gmail.com>
2014-07-11 12:40:34 -0700

    Switched to MQ API call

--- www/js/geoLocate.js
+++ www/js/geoLocate.js
@@ -1,25 +1,19 @@
-var map = null;
+
 var geocoder = null;
 
 function initialize() {
-  if (GBrowserIsCompatible()) {
-    geocoder = new GClientGeocoder();
+  geocoder = new MQA.Geocoder();
+}
+
+function responseHandler( point ) {
+  if( !point ) {
+    alert(address + " not found");
+  }
+  else {
+    document.getElementById('geolocation').innerHTML += '<p>Your location: ' + point + '</p>';
   }
 }
 
-function showAddress(address) {
-  if (geocoder) {
-    geocoder.getLatLng(
-      address,
-      function(point) {
-        if (!point) {
-          alert(address + " not found");
-          
-        } else {
-          document.getElementById('geolocation').innerHTML += '<p>Your location: ' + point + '</p>';
-
-        }
-      }
-    );
-  }
+function showAddress( address ) {
+  geocoder.geocode( address, { maxResults: 1 }, null, responseHandler );
 }
