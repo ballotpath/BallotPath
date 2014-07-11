f872d0b449c220e3df935b55e6bd3a61f45b470b
mclyde <mclyde122@gmail.com>
2014-07-11 14:48:58 -0700

    Update gelocate functions to handle MQA response

--- www/js/geoLocate.js
+++ www/js/geoLocate.js
@@ -10,8 +10,12 @@ function responseHandler( point ) {
     alert(address + " not found");
   }
   else {
-    document.getElementById('geolocation').innerHTML += '<p>Your location: ' + point + '</p>';
+    //document.getElementById('geolocation').innerHTML += '<p>Your location: ' + point + '</p>';
+    var location = point.results[0].locations[0];
+    var url = window.location.protocol + "//" + window.location.host + "/office/" + location.latLng.lat + "/" + location.latLng.lng;
+    window.open(url, "_self");
   }
+  return false;
 }
 
 function showAddress( address ) {
