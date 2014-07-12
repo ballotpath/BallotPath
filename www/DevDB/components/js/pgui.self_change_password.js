50677efc6876c573faad85fafab43bc0a2f3f7bc
David Clough <davidclough@outlook.com>
2014-07-11 19:40:27 -0700

    Really deleted all the PHP files.

--- www/DevDB/components/js/pgui.self_change_password.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    PhpGenChangePasswordDialog.initialize(new PhpGenChangePasswordDialogUserStrategy(
-        function(currentPassword, newPassword) {
-          return PhpGenUserManagementApi.selfChangePassword(currentPassword, newPassword);
-    }));
-    $('#self-change-password').click(function() {PhpGenChangePasswordDialog.open();});
-})();
-
