e764fd27f3e39bd90935094bb6f37a768f1bb245
mclyde <mclyde122@gmail.com>
2014-07-12 08:52:49 -0700

    Added wrapper function

--- www/js/geoLocate.js
+++ www/js/geoLocate.js
@@ -1,17 +1,11 @@
 
-var geocoder = null;
-
-function initialize() {
-  geocoder = new MQA.Geocoder();
-}
-
 function responseHandler( point ) {
   if( !point ) {
-    alert(address + " not found");
+    alert("Address not found");
   }
   else {
-    //document.getElementById('geolocation').innerHTML += '<p>Your location: ' + point + '</p>';
     var location = point.results[0].locations[0];
+    //document.getElementById('geolocation').innerHTML = '<p>Your location: ' + location.latLng.lat + ", " + location.latLng.lng + '</p>';
     var url = window.location.protocol + "//" + window.location.host + "/office/" + location.latLng.lat + "/" + location.latLng.lng;
     window.open(url, "_self");
   }
@@ -19,5 +13,7 @@ function responseHandler( point ) {
 }
 
 function showAddress( address ) {
-  geocoder.geocode( address, { maxResults: 1 }, null, responseHandler );
+  MQA.withModule('geocoder', function() {
+    MQA.Geocoder.geocode( address, { maxResults: 1 }, null, responseHandler );
+  });
 }
