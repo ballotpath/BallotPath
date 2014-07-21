8660dd688126e5c85af95d2f96076fb909ce0ff4
Shawn Forgie <sforgie@ip-172-31-10-241.us-west-2.compute.internal>
2014-07-21 00:11:37 +0000

    CSV Validation complete, imports work smoothly and error messages returned in files.

--- app/viewsd/bulkimport.py
+++ app/viewsd/bulkimport.py
@@ -14,23 +14,32 @@ hol_pos_off_header = ['first_name', 'middle_name', 'last_name', 'holder_addr1', 
 el_div_dist_header = ['district_name', 'district_state', 'level_name', 'election_div_name', 'phys_addr1', 'phys_addr2', 'phys_addr_city', 'phys_addr_state', 'phys_addr_zip', 'mail_addr1', 'mail_addr2', 'mail_addr_city', 'mail_addr_state', 'mail_addr_zip', 'election_div_phone', 'fax', 'election_div_website', 'election_div_doc_name', 'election_div_doc_link']
 
 def begin(filename):
+    """ 
+    Start the importation process of a csv file located in the upload folder.
+    Returns an empty string or the name of a file containing error messages.
+
+    Arguments
+    ---------
+
+    'filename' - the name of the file that is to be validated and then imported
+
+    """
     result = ''
     try:
-        impfile = open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        reader = csv.reader(impfile, delimiter='|', dialect=csv.excel)
-        headers = reader.next()
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as impfile:
+            reader = csv.reader(impfile, delimiter='|', dialect=csv.excel)
+            headers = reader.next()
     except IOError:
         #file could not be read
         result = validation_error("File could not be opened!")
 
     if headers is not None:
         if headers == hol_pos_off_header:
-            result = validate_holder_position_office(reader, filename)
+            result = validate_holder_position_office(filename)
         else:
             if headers == el_div_dist_header:
-                result = validate_election_division_district(reader, filename)
-            else:
-		print "headers don't match"
+                result = validate_election_division_district(filename)
+            else:	
                 #file headers do not match what we want
                 result = validation_error("File headers do not match required format!\nReceived:    " + str(headers) + "Expected:   " + str(hol_pos_off_header) + "\nOR\n" + str(el_div_dist_header))
 
@@ -40,25 +49,29 @@ def begin(filename):
 
 
 # Validation for the holder, postion and office csv file
-def validate_holder_position_office(reader, filename):
+def validate_holder_position_office(filename):
+    """
+    Validate holders, positions and offices fields.
+
+    Arguments
+    ---------
+
+    'filename' - the name of the file to be validated, expected location is in the 'UPLOAD_FOLDER'
+
+    """
     validator = CSVValidator(hol_pos_off_header)
     validator.add_header_check('EX1', 'bad header')
-    validator.add_record_check(check_holder_addr_string_length)
-
-    validator.add_record_check(check_valid_dates_holder)
-    #datemsg = ' should be in the format: month, dd yyyy'
-    #validator.add_value_check('term_start', datetime_string('%m %d, %Y'), 'EX2', 'term_start'+datemsg)
-    #validator.add_value_check('term_end', datetime_string('%m %d, %Y'), 'EX3', 'term_end'+datemsg)
-    #validator.add_value_check('filing_deadline', datetime_string('%m %d, %Y'), 'EX4', 'filing_deadline'+datemsg)
-    #validator.add_value_check('next_election', datetime_string('%m %d, %Y'), 'EX5', 'next_election'+datemsg)
-
     validator.add_value_check('term_length_months', int, 'EX2', 'term_length_months must be an integer')
-    validator.add_value_check('partisan', bool, 'EX3', 'partisan must be True or False')
+    validator.add_record_check(check_partisan_boolean)
     validator.add_value_check('salary', int, 'EX4', 'salary must be an integer')
+    validator.add_record_check(check_holder_addr_string_length)
+    validator.add_record_check(check_valid_dates_holder)
+    validator.add_record_check(check_holder_not_null_fields)
 
-    problems = validator.validate(reader)
+    data = csv.reader(open(os.path.join(app.config['UPLOAD_FOLDER'], filename)), delimiter='|', dialect=csv.excel)
+    problems = validator.validate(data)
 
-    if problems is not None:
+    if problems is not None and problems != []:
         fname = 'bad_inserts_'+ time.strftime("%Y-%m-%d-%H_%M_%S") + '.csv'
         efile = open(os.path.join(app.config['ERROR_FOLDER'], fname), 'w')
         write_problems(problems, efile)
@@ -67,30 +80,84 @@ def validate_holder_position_office(reader, filename):
     return import_holder_position_office(filename)
 
 
+def check_holder_not_null_fields(r):
+    """
+    Check that required holder_position_office.csv fields are not empty
+    """
+    dist_name = str(r['district_name'])
+    dist_state = str(r['district_state'])
+    el_name = str(r['election_div_name'])   
+
+    dname_valid = (dist_name.strip())
+    dstate_valid = (dist_state.strip())
+    ename_valid = (el_name.strip()) 
+
+    msg = ''
+    if not dname_valid:
+        msg = 'district_name: {0}\n'.format(dist_name)
+    if not dstate_valid:
+        msg += 'district_state: {0}\n'.format(dist_state)
+    if not ename_valid:
+        msg += 'election_div_name: {0}\n'.format(el_name)
+
+    if msg:
+       raise RecordError('EX7', 'Expected non-empty string:\n{0}'.format(msg))
+
+
+def check_partisan_boolean(r):
+    """
+    Check that partisan field is a valid boolean value that can be interpreted by postgresql
+    """
+    partisan = str(r['partisan']).strip()
+    
+    valid = partisan in ['TRUE', 'True', 't', 'true', 'y', 'yes', 'on', '1', 'FALSE', 'False', 'f', 'false', 'n', 'no', 'off', '0']
+    if not valid:
+        raise RecordError('EX3', 'partisan must be True or False. Encountered: {0}'.format(partisan))
+
+
 def check_holder_addr_string_length(r):
+    """
+    Check that address fields are no more than 25 characters long
+    Note: This check may be removed later if we change db to accept longer addresses
+    """
     addr1 = str(r['holder_addr1'])
     addr2 = str(r['holder_addr2'])
     valid = (len(addr1) <= 25 and len(addr2) <= 25)
     if not valid:
-        raise RecordError('EX5', 'holder_addr1 or holder_addr2 length is too long, must be less than or equal to 25 characters')
-
+        raise RecordError('EX5', 'Length is too long, must be less than or equal to 25 characters\nFor holder_addr1: {0}\nOR\nholder_addr2: {1}'.format(addr1, addr2))
 
 
 def check_valid_dates_holder(r):
-    start = str(r['term_start'])
-    end = str(r['term_end'])
-    f_deadline = str(r['filing_deadline'])
-    next_el = str(r['next_election'])
+    """
+    Check that date fields are in expected format.
+    ex: 'Jan 12, 2012' or 'January 12, 2012'
+    Really any format that postgres could interpret could be accepted but we will limit it for now.
+    """
+    start = str(r['term_start']).strip()
+    end = str(r['term_end']).strip()
+    f_deadline = str(r['filing_deadline']).strip()
+    next_el = str(r['next_election']).strip()
 
     valid = ((try_parse_date(start, '%b %d, %Y') or try_parse_date(start, '%B %d, %Y')) 
               and (try_parse_date(end, '%b %d, %Y') or try_parse_date(end, '%B %d, %Y'))
               and (try_parse_date(f_deadline, '%b %d, %Y') or try_parse_date(f_deadline, '%B %d, %Y'))
               and (try_parse_date(next_el, '%b %d, %Y') or try_parse_date(next_el, '%B %d, %Y')))
     if not valid:
-        raise RecordError('EX6', 'Incorrect date format: ' +  start + ', ' + end + ', ' + f_deadline + ', ' + next_el)
+        raise RecordError('EX6', 'Incorrect date format in one of the following: {0}, {1}, {2}, {3}\nValid format examples are "Jan 22, 2012" or "January 22, 2012"'.format(start, end, f_deadline, next_el))
 
 
 def try_parse_date(s, f):
+    """
+    Try to parse a datetime string with a given format.
+    Returns a boolean true if successful, false if not.
+
+    Arguments
+    ---------
+
+    's' - string to parse
+    'f' - format to parse date as
+
+    """
     if s is not None:
         try: 
             datetime.strptime(s, f)
@@ -99,31 +166,119 @@ def try_parse_date(s, f):
     return True
 
 
-# Validation for the election division and district csv file
-def validate_election_division_district(reader, filename):
-    
+def validate_election_division_district(filename):
+    """
+    Validate district and election_division fields.
+
+    Arguments
+    ---------
+
+    'filename' - the name of the file to validate, expected location is 'UPLOAD_FOLDER'
+
+    """
+    validator = CSVValidator(el_div_dist_header)
+    validator.add_header_check('EX1', 'bad header')
+    validator.add_record_check(check_election_div_addr_length)
+    validator.add_record_check(check_election_div_not_null_fields)
+
+    data = csv.reader(open(os.path.join(app.config['UPLOAD_FOLDER'], filename)), delimiter='|', dialect=csv.excel)
+    problems = validator.validate(data)
+
+    if problems is not None and problems != []:
+        fname = 'bad_inserts_'+ time.strftime("%Y-%m-%d-%H_%M_%S") + '.csv'
+        efile = open(os.path.join(app.config['ERROR_FOLDER'], fname), 'w')
+        write_problems(problems, efile)
+        return fname
+
     return import_election_division_district(filename)
     
 
+def check_election_div_not_null_fields(r):
+    """
+    Check that the required fields of the election division and district csv are not empty
+    """
+    dist_name = str(r['district_name'])
+    dist_state = str(r['district_state'])
+    el_name = str(r['election_div_name'])
+    el_state = str(r['phys_addr_state'])
+
+    dname_valid = (dist_name.strip())
+    dstate_valid = (dist_state.strip())
+    ename_valid = (el_name.strip())
+    estate_valid = (el_state.strip())
+
+    msg = ''
+    if not dname_valid:
+        msg = 'district_name: {0}\n'.format(dist_name)
+    if not dstate_valid:
+        msg += 'district_state: {0}\n'.format(dist_state)
+    if not ename_valid:
+        msg += 'election_div_name: {0}\n'.format(el_name)
+    if not estate_valid:
+        msg += 'phys_addr_state: {0}\n'.format(el_state)
+
+    if msg:
+       raise RecordError('EX3', 'Expected non-empty string:\n{0}'.format(msg))
+
+
+def check_election_div_addr_length(r):
+    """
+    Check to see if address fields have too many characters.
+    For no the db has a 25 character limit.
+    """
+    phys_addr1 = str(r['phys_addr1'])
+    phys_addr2 = str(r['phys_addr2'])
+    mail_addr1 = str(r['mail_addr1'])
+    mail_addr2 = str(r['mail_addr2'])
+    
+    p1_valid = (len(phys_addr1) <= 25)
+    p2_valid = (len(phys_addr2) <= 25)
+    m1_valid = (len(phys_addr1) <= 25)
+    m2_valid = (len(phys_addr2) <= 25)
+    
+    msg = ''
+
+    if not p1_valid:
+        msg = 'phys_addr1: ' + phys_addr1 + '\n'
+    if not p2_valid:
+        msg += 'phys_addr2: ' + phys_addr2 + '\n'
+    if not m1_valid:
+        msg += 'mail_addr1: ' + mail_addr1 + '\n'
+    if not m2_valid:
+        msg += 'mail_addr2: ' + mail_addr2
+
+    if msg:
+        raise RecordError('EX2', 'Address length is too long expected 25 characters max\n{0}'.format(msg))
+
+
 def import_holder_position_office(name):
+    """
+    Call the stored procedure to import office holders, positions and offices from a validated csv.
+    """
     try:
         result = db.engine.execute(text('SELECT bp_import_off_pos_hol_csv_to_staging_tables(:filename);'), filename=name)
         for row in result:
             return row['bp_import_off_pos_hol_csv_to_staging_tables']
     except SQLAlchemyError as sqle:
-        return validation_error(str(sqle).split("SQL statement")[0])
+        return sql_validation_error(str(sqle).split("SQL statement")[0])
 
 
 def import_election_division_district(name):
+    """
+    Call the stored procedure to import a validated election division and district csv.
+    """
     try:
         result = db.engine.execute(text('SELECT bp_import_dist_elec_div_csv_to_staging_tables(:filename);'), filename=name)
         for row in result:
             return row['bp_import_dist_elec_div_csv_to_staging_tables']  
     except SQLAlchemyError as sqle:
-        return validation_error(str(sqle).split("SQL statement")[0])
+        return sql_validation_error(str(sqle).split("SQL statement")[0])
 
 
 def cleanup(filename):
+    """
+    Remove the uploaded file from the system.
+    """
     rfile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     try:  
         if os.path.exists(rfile):
@@ -132,9 +287,18 @@ def cleanup(filename):
         pass
 
 
-def validation_error(error):
+def sql_validation_error(error):
+    """
+    Write an error message to a file that can be returned to the client
+
+    Arguments
+    ---------
+
+    'error' - the error message to write
+
+    """
     filename = 'bad_inserts_'+ time.strftime("%Y-%m-%d-%H_%M_%S") + '.csv'
     efile = os.path.join(app.config['ERROR_FOLDER'], filename)
-    errorfile = open(efile, 'w')
-    errorfile.write(error)
+    with  open(efile, 'w') as errorfile:
+        errorfile.write(error)
     return filename
