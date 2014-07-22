9791a84ff0f7b040c98fc855770555b159fd2158
Andrew Hobbs <firedraco2@yahoo.com>
2014-07-22 06:25:04 +0000

    Fixed office_get_positions to use the actual stored procedure.

--- api/app/viewsd/office.py
+++ api/app/viewsd/office.py
@@ -7,60 +7,104 @@ import json
 def parse_office_row(row):
     # First, extract office position information from the query
     office_pos = {}
-    office_pos['id'] = row.office_position.id
-    office_pos['district_id'] = row.office_position.district_id
-    office_pos['office_id'] = row.office_position.office_id
-    office_pos['office_holder_id'] = row.office_position.office_holder_id
-    office_pos['position_name'] = row.office_position.position_name
-    office_pos['term_start'] = str(row.office_position.term_start)
-    office_pos['term_end'] = str(row.office_position.term_end)
-    office_pos['filing_deadline'] = str(row.office_position.filing_deadline)
-    office_pos['next_election'] = str(row.office_position.next_election)
-    office_pos['notes'] = row.office_position.notes
+    office_pos['id'] = row.office_position_id
+    office_pos['district_id'] = row.office_position_district_id
+    office_pos['office_id'] = row.office_position_office_id
+    office_pos['office_holder_id'] = row.office_position_office_holder_id
+    office_pos['position_name'] = row.office_position_position_name
+    office_pos['term_start'] = str(row.office_position_term_start)
+    office_pos['term_end'] = str(row.office_position_term_end)
+    office_pos['filing_deadline'] = str(row.office_position_filing_deadline)
+    office_pos['next_election'] = str(row.office_position_next_election)
+    office_pos['notes'] = row.office_position_notes
     # Next, extract office information
     office = {}
-    office['id'] = row.office.id
-    office['title'] = row.office.title
-    office['num_positions'] = row.office.num_positions
-    office['responsibilities'] = row.office.responsibilities
-    office['term_length_months'] = row.office.term_length_months
-    office['filing_fee'] = row.office.filing_fee
-    office['partisan'] = row.office.partisan
-    office['age_requirements'] = row.office.age_requirements
-    office['res_requirements'] = row.office.res_requirements
-    office['prof_requirements'] = row.office.prof_requirements
-    office['salary'] = row.office.salary
-    office['notes'] = row.office.notes
+    office['id'] = row.office_id
+    office['title'] = row.office_title
+    office['num_positions'] = row.office_num_positions
+    office['responsibilities'] = row.office_responsibilities
+    office['term_length_months'] = row.office_term_length_months
+    office['filing_fee'] = row.office_filing_fee
+    office['partisan'] = row.office_partisan
+    office['age_requirements'] = row.office_age_requirements
+    office['res_requirements'] = row.office_res_requirements
+    office['prof_requirements'] = row.office_prof_requirements
+    office['salary'] = row.office_salary
+    office['notes'] = row.office_notes
     # Put it in as a sub-object
     office_pos['office'] = office
     # Finally, extract office_holder information
     office_holder = {}
-    office_holder['id'] = row.office_holder.id
-    office_holder['first_name'] = row.office_holder.first_name
-    office_holder['middle_name'] = row.office_holder.middle_name
-    office_holder['last_name'] = row.office_holder.last_name
-    office_holder['party_affiliation'] = row.office_holder.party_affiliation
-    office_holder['address1'] = row.office_holder.address1
-    office_holder['address2'] = row.office_holder.address2
-    office_holder['city'] = row.office_holder.city
-    office_holder['state'] = row.office_holder.state
-    office_holder['zip'] = row.office_holder.zip
-    office_holder['phone'] = row.office_holder.phone
-    office_holder['fax'] = row.office_holder.fax
-    office_holder['email_address'] = row.office_holder.email_address
-    office_holder['website'] = row.office_holder.website
-    office_holder['photo_link'] = row.office_holder.photo_link
-    office_holder['notes'] = row.office_holder.notes
+    office_holder['id'] = row.office_holder_id
+    office_holder['first_name'] = row.office_holder_first_name
+    office_holder['middle_name'] = row.office_holder_middle_name
+    office_holder['last_name'] = row.office_holder_last_name
+    office_holder['party_affiliation'] = row.office_holder_party_affiliation
+    office_holder['address1'] = row.office_holder_address1
+    office_holder['address2'] = row.office_holder_address2
+    office_holder['city'] = row.office_holder_city
+    office_holder['state'] = row.office_holder_state
+    office_holder['zip'] = row.office_holder_zip
+    office_holder['phone'] = row.office_holder_phone
+    office_holder['fax'] = row.office_holder_fax
+    office_holder['email_address'] = row.office_holder_email_address
+    office_holder['website'] = row.office_holder_website
+    office_holder['photo_link'] = row.office_holder_photo_link
+    office_holder['notes'] = row.office_holder_notes
     # And also add it as a sub-object
     office_pos['office_holder'] = office_holder
     return office_pos
-
 # Office:
-@app.route("/office/<float:latitude>/<float:longitude>/")
+@app.route("/office/<latitude>/<longitude>/")
 def get_offices(latitude, longitude, methods = ['GET']):
     office_positions = []
-    # For now, just query all office_positions
-    result = db.session.query(models.office, models.office_holder, models.office_position).filter(models.office.id == models.office_position.office_id).filter(models.office_holder.id == models.office_position.id).all()
+    #result = db.session.query(models.office, models.office_holder, models.office_position).filter(models.office.id == models.office_position.office_id).filter(models.office_holder.id == models.office_position.id).all()
+    cmd = """
+SELECT office_position.id as office_position_id
+       , office_position.district_id as office_position_district_id
+       , office_position.office_id as office_position_office_id
+       , office_position.office_holder_id as office_position_office_holder_id
+       , office_position.position_name as office_position_position_name
+       , office_position.term_start as office_position_term_start
+       , office_position.term_end as office_position_term_end
+       , office_position.filing_deadline as office_position_filing_deadline
+       , office_position.next_election as office_position_next_election
+       , office_position.notes as office_position_notes
+    --OFFICES
+       , office.id as office_id
+       , office.title as office_title
+       , office.num_positions as office_num_positions
+       , office.responsibilities as office_responsibilities
+       , office.term_length_months as office_term_length_months
+       , office.filing_fee as office_filing_fee
+       , office.partisan as office_partisan
+       , office.age_requirements as office_age_requirements
+       , office.res_requirements as office_res_requirements
+       , office.prof_requirements as office_prof_requirements
+       , office.salary as office_salary
+       , office.notes as office_notes
+    --OFFICEHOLDER
+       , office_holder.id as office_holder_id
+       , office_holder.first_name as office_holder_first_name
+       , office_holder.middle_name as office_holder_middle_name
+       , office_holder.last_name as office_holder_last_name
+       , office_holder.party_affiliation as office_holder_party_affiliation
+       , office_holder.address1 as office_holder_address1
+       , office_holder.address2 as office_holder_address2
+       , office_holder.city as office_holder_city
+       , office_holder.state as office_holder_state
+       , office_holder.zip as office_holder_zip
+       , office_holder.phone as office_holder_phone
+       , office_holder.fax as office_holder_fax
+       , office_holder.email_address as office_holder_email_address
+       , office_holder.website as office_holder_website
+       , office_holder.photo_link as office_holder_photo_link
+       , office_holder.notes as office_holder_notes
+   FROM bp_get_officeids_from_point("""+str(longitude)+", "+str(latitude)+""") sp 
+       JOIN office_position ON sp.district_id = office_position.district_id
+       JOIN office ON office_position.office_id = office.id
+       JOIN office_holder ON office_position.office_holder_id = office_holder.id"""
+    result = db.session.execute(cmd)
     for row in result:
         office_positions.append(parse_office_row(row))
     return Response(json.dumps({ "office_positions" : office_positions }), status=200, mimetype='application/json')
@@ -69,6 +113,9 @@ def get_offices(latitude, longitude, methods = ['GET']):
 def get_office(office_id):
     # Use our model to connect to the DB and get our particular office
     office = models.office.query.get(office_id)
+    result = db.session.execute("SELECT id as office_id FROM office WHERE id=1;")
+    for row in result:
+        error.text
     if office == None:
         return Response(json.dumps(None), status=404, mimetype='application/json')
     else:
