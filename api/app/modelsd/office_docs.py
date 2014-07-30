3f814d73d28a302799002615d77d87b3ab6a421b
mclyde <mclyde@pdx.edu>
2014-07-29 17:36:03 -0700

    Revert "Updated Flask DB Models to fit with changes Shawn made."

--- api/app/modelsd/office_docs.py
+++ api/app/modelsd/office_docs.py
@@ -4,5 +4,5 @@ from sqlalchemy.dialects import postgresql
 class office_docs(db.Model):
     id = db.Column(db.INTEGER, primary_key = True)
     office_id = db.Column(db.INTEGER, db.ForeignKey('office.id'))
-    name = db.Column(db.VARCHAR(125), nullable = False)
+    name = db.Column(db.VARCHAR(35), nullable = False)
     link = db.Column(db.TEXT, nullable = False)
