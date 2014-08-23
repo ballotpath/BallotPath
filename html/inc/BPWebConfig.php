dfc852d331ca733c416c9f45aa3813968698456a
darrell <djs6255@gmail.com>
2014-08-23 12:28:44 -0700

    Accidentally deleted BPWebConfig from repo

---
+++ html/inc/BPWebConfig.php
@@ -0,0 +1,11 @@
+<?php
+//Configuration file for web interface
+$webaddr="http://someurl.com/";  //base url
+$dbhost="localhost";  
+$dbport=5432;
+$dbname="Database";
+$dbuser="User";
+$dbpassword="Pass";
+
+
+?>
